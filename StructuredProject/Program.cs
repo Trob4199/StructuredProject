140c89f92ee34bd29d94d1f0bfa18a97e406c879
Trevor Roberts <trob4199@gmail.com>
2021-07-26 20:30:00 -0600

    Add project files.

---
+++ StructuredProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StructuredProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
